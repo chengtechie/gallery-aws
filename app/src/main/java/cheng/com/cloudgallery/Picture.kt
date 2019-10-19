8978ea9f70611aa6593d0f50e3b7a6192e9788c6
chengchinlim <chengchin5227@gmail.com>
2019-10-18 23:16:14 -0700

    Add Picture model

--- app/src/main/java/cheng/com/cloudgallery/Picture.kt
+++ app/src/main/java/cheng/com/cloudgallery/Picture.kt
@@ -1,7 +1,24 @@
 package cheng.com.cloudgallery
 
-class Picture(private val id: Int, private val ut: String, private val desc: String) {
-    var drawableId = id
-    var uploadTime = ut
-    var description = desc
+import android.graphics.Bitmap
+
+class Picture() {
+
+    var drawableId: Int? = null
+    var uploadTime: String? = null
+    var description: String? = null
+    var bitmap: Bitmap? = null
+
+    constructor(id: Int, ut: String, desc: String): this() {
+        drawableId = id
+        uploadTime = ut
+        description = desc
+    }
+
+    constructor(bm: Bitmap, ut: String, desc: String): this(){
+        bitmap = bm
+        uploadTime = ut
+        description = desc
+    }
+
 }
